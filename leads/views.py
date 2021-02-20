8967a95179bab2899a99f26b42a04c458a64019c
İlter <ilterkose98@icloud.com>
2021-02-20 23:43:20 +0300

    feat: lead form added

--- leads/views.py
+++ leads/views.py
@@ -1,6 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 from django.http import HttpResponse
-from .models import Lead
+from .models import Lead, Agent
+from .forms import LeadForm
 
 
 # Create your views here.
@@ -22,4 +23,27 @@ def lead_detail(request, pk):
     context = {
         "lead": lead
     }
-    return render(request,"leads/lead_detail.html",context=context)
+    return render(request, "leads/lead_detail.html", context=context)
+
+
+def lead_create(request):
+    form = LeadForm()
+    if request.method == "POST":
+        form = LeadForm(request.POST)
+        if form.is_valid():
+            print(f"FORM IS VALID {form.cleaned_data}")
+            first_name = form.cleaned_data["first_name"]
+            last_name = form.cleaned_data["last_name"]
+            age = form.cleaned_data["age"]
+            agent = Agent.objects.first()
+            Lead.objects.create(
+                first_name=first_name,
+                last_name=last_name,
+                age=age,
+
+            )
+            return redirect('/leads')
+    context = {
+        "form": form
+    }
+    return render(request, "leads/lead_create.html", context=context)
