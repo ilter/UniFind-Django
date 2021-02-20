6477f11893683c7d80f217878edde66c0098d91d
İlter <ilterkose98@icloud.com>
2021-02-20 17:14:26 +0300

    feat: agent created and connected w/ foreign key

--- leads/models.py
+++ leads/models.py
@@ -2,6 +2,12 @@ from django.db import models
 
 
 # Create your models here.
+
+class Agent(models.Model):
+    first_name = models.CharField(max_length=30)
+    last_name = models.CharField(max_length=30)
+
+
 class Lead(models.Model):
     SOURCE_CHOICES = (
         ('YT', 'YouTube'),  # first value to store db second value for view
@@ -12,6 +18,8 @@ class Lead(models.Model):
     first_name = models.CharField(max_length=30)
     last_name = models.CharField(max_length=30)
     age = models.IntegerField(default=0)
+    agent = models.ForeignKey('Agent', on_delete=models.SET_NULL, null=True)
+    # if u want on_delete makes it null have to declare null=True
 
     phoned = models.BooleanField(default=False)
     source = models.CharField(choices=SOURCE_CHOICES, max_length=100)
