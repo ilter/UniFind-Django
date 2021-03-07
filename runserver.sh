5d4e92de828c3295c56cdcacdb6a9e0a4b96becc
İlter <ilterkose98@icloud.com>
2021-03-07 16:47:34 +0300

    fix: runserver-update

--- runserver.sh
+++ runserver.sh
@@ -2,4 +2,4 @@ python manage.py collectstatic --no-input
 
 python manage.py migrate
 
-gunicorn --worker-tmp-dir /dev/shm DjangoCRM.wsgi
+gunicorn --worker-tmp-dir /dev/shm -b DjangoCRM.wsgi:application
